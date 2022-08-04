91af8e8c56cdb8d559a808cff9c173d372cf6a64
Maria Gragera <m.gragera.garces@gmail.com>
2022-08-04 14:37:51 +0100

    networkx to ns empty files

---
+++ Networkx to Netsquid graph/networkx_generator.py
@@ -0,0 +1,9 @@
+#Complete multipartite graph with the specified block sizes
+
+import networkx as nx #networkx lib
+import matplotlib.pyplot as plt
+from Model_variables import bl_sz
+
+I= nx.complete_multipartite_graph(bl_sz+1) 
+nx.draw(I, with_labels=True)
+plt.show()
