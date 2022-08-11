19cd4061852b29968c596bdfb4f92f10ac37b7de
Maria Gragera <m.gragera.garces@gmail.com>
2022-08-11 11:08:18 +0100

    connections

---
+++ Main.py/connections.py
@@ -0,0 +1,64 @@
+import netsquid as ns
+import netsquid.qubits.ketstates as ks #qubit state format - can be modif
+from netsquid.components import ClassicalChannel #input standart classical channel class 
+from netsquid.components import QuantumChannel, FixedDelayModel
+from netsquid.components.models import FibreDelayModel #chosen connection model for classical connections - can be modif
+from netsquid.nodes.connections import Connection
+from netsquid.qubits.qubitapi import *
+from netsquid.qubits.qformalism import *
+from netsquid.nodes import Node, Network 
+
+#Quantum connection
+class EntanglingConnection(Connection):
+    """A connection that generates entanglement.
+
+    Consists of a midpoint holding a quantum source that connects to
+    outgoing quantum channels.
+
+    Parameters
+    ----------
+    length : float
+        End to end length of the connection [km].
+    source_frequency : float
+        Frequency with which midpoint entanglement source generates entanglement [Hz].
+    name : str, optional
+        Name of this connection.
+
+    """
+
+    def __init__(self, length, source_frequency, name="EntanglingConnection"):
+        super().__init__(name=name)
+        qsource = QSource(f"qsource_{name}", StateSampler([ks.b00], [1.0]), num_ports=2,
+                          timing_model=FixedDelayModel(delay=1e9 / source_frequency),
+                          status=SourceStatus.INTERNAL)
+        self.add_subcomponent(qsource, name="qsource")
+        qchannel_c2a = QuantumChannel("qchannel_C2A", length=length / 2,
+                                      models={"delay_model": FibreDelayModel()})
+        qchannel_c2b = QuantumChannel("qchannel_C2B", length=length / 2,
+                                      models={"delay_model": FibreDelayModel()})
+        # Add channels and forward quantum channel output to external port output:
+        self.add_subcomponent(qchannel_c2a, forward_output=[("A", "recv")])
+        self.add_subcomponent(qchannel_c2b, forward_output=[("B", "recv")])
+        # Connect qsource output to quantum channel input:
+        qsource.ports["qout0"].connect(qchannel_c2a.ports["send"])
+        qsource.ports["qout1"].connect(qchannel_c2b.ports["send"])
+
+#Classical connection
+class ClassicalConnection(Connection):
+    """A connection that transmits classical messages in one direction, from A to B.
+
+    Parameters
+    ----------
+    length : float
+        End to end length of the connection [km].
+    name : str, optional
+       Name of this connection.
+
+    """
+
+    def __init__(self, length, name="ClassicalConnection"):
+        super().__init__(name=name)
+        self.add_subcomponent(ClassicalChannel("Channel_A2B", length=length,
+                                               models={"delay_model": FibreDelayModel()}),
+                              forward_input=[("A", "send")],
+                              forward_output=[("B", "recv")])
--- Main.py/network.py
+++ Main.py/network.py
@@ -1,3 +1,4 @@
+#netsquid imports
 import netsquid as ns
 import netsquid.components.instructions as instr #instructions that can be followed by the processor
 import netsquid.qubits.ketstates as ks #qubit state format - can be modif
@@ -11,6 +12,11 @@ from netsquid.qubits.qubitapi import *
 from netsquid.qubits.qformalism import *
 from netsquid.nodes import Node, Network 
 
+#import from connections
+from connections import EntanglingConnection #Quantum channel
+from connections import ClassicalConnection #Classical channel
+
+
 def Two_node_network(num_qubits=3):
 
     network = Network('SimpleLinkNetwork')
@@ -20,7 +26,7 @@ def Two_node_network(num_qubits=3):
         node.add_subcomponent(QuantumProcessor(f'qmem_{node.name}',
                                                num_positions=num_qubits + 1,
                                                fallback_to_nonphysical=True))
-    conn = HeraldedConnection("HeraldedConnection", length_to_a=distance / 2,
+    conn = EntanglingConnection("EntanglingConnection", length_to_a=distance / 2,
                               length_to_b=distance / 2, time_window=20)
     network.add_connection(nodes[0], nodes[1], connection=conn, label='quantum')
     network.add_connection(nodes[0], nodes[1], delay=distance / 200000 * 1e9, label='classical')
