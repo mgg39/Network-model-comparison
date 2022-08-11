b42c1b591b01439805cf72bcc54f162e6aea6359
Maria Gragera <m.gragera.garces@gmail.com>
2022-08-11 12:59:18 +0100

    repeater sub-protocol for middle nodes

---
+++ Main.py/repeater.py
@@ -0,0 +1,25 @@
+def setup_repeater_protocol(network):
+    """Setup repeater protocol on repeater chain network.
+
+    Parameters
+    ----------
+    network : :class:`~netsquid.nodes.network.Network`
+        Repeater chain network to put protocols on.
+
+    Returns
+    -------
+    :class:`~netsquid.protocols.protocol.Protocol`
+        Protocol holding all subprotocols used in the network.
+
+    """
+    protocol = LocalProtocol(nodes=network.nodes)
+    # Add SwapProtocol to all repeater nodes. Note: we use unique names,
+    # since the subprotocols would otherwise overwrite each other in the main protocol.
+    nodes = [network.nodes[name] for name in sorted(network.nodes.keys())]
+    for node in nodes[1:-1]:
+        subprotocol = SwapProtocol(node=node, name=f"Swap_{node.name}")
+        protocol.add_subprotocol(subprotocol)
+    # Add CorrectProtocol to Bob
+    subprotocol = CorrectProtocol(nodes[-1], len(nodes))
+    protocol.add_subprotocol(subprotocol)
+    return protocol
