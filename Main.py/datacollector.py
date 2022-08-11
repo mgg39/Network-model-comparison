b34f7bef9cc8bd3383bef881a11a18995061e21c
Maria Gragera <m.gragera.garces@gmail.com>
2022-08-11 12:58:38 +0100

    data collector

---
+++ Main.py/datacollector.py
@@ -0,0 +1,31 @@
+def setup_datacollector(network, protocol):
+    """Setup the datacollector to calculate the fidelity
+    when the CorrectionProtocol has finished.
+
+    Parameters
+    ----------
+    network : :class:`~netsquid.nodes.network.Network`
+        Repeater chain network to put protocols on.
+
+    protocol : :class:`~netsquid.protocols.protocol.Protocol`
+        Protocol holding all subprotocols used in the network.
+
+    Returns
+    -------
+    :class:`~netsquid.util.datacollector.DataCollector`
+        Datacollector recording fidelity data.
+
+    """
+    # Ensure nodes are ordered in the chain:
+    nodes = [network.nodes[name] for name in sorted(network.nodes.keys())]
+
+    def calc_fidelity(evexpr):
+        qubit_a, = nodes[0].qmemory.peek([0])
+        qubit_b, = nodes[-1].qmemory.peek([1])
+        fidelity = ns.qubits.fidelity([qubit_a, qubit_b], ks.b00, squared=True)
+        return {"fidelity": fidelity}
+
+    dc = DataCollector(calc_fidelity, include_entity_name=False)
+    dc.collect_on(pydynaa.EventExpression(source=protocol.subprotocols['CorrectProtocol'],
+                                          event_type=Signals.SUCCESS.value))
+    return dc
