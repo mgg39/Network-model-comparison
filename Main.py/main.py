fef5257891c3b9b49af0a79bba47e3e4c973d1a3
Maria Gragera <m.gragera.garces@gmail.com>
2022-08-11 10:37:10 +0100

    initializing network

--- Main.py/main.py
+++ Main.py/main.py
@@ -5,10 +5,23 @@ from netsquid.util import DataCollector
 from netsquid.qubits.qubitapi import *
 from netsquid.qubits.qformalism import *
 from netsquid.nodes import Node, Network 
-from netsquid.protocols import 
+from netsquid.protocols import NodeProtocol
 
 
 #import topology 
 from network import Two_node_network
 #import protocol 
 from protocol import Forward_message
+
+#Common warning
+#np.warnings.filterwarnings('ignore', category=np.VisibleDeprecationWarning)
+
+def Run_experiment(num_qubits):
+
+    #Initialize network
+    network = Two_node_network()
+    protocols = []
+    measure_protocols = []
+    measure_node_nums = []
+
+    
