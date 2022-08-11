d2be9fdede817467f08fbb6c9a1be4e73e2fb70a
Maria Gragera <m.gragera.garces@gmail.com>
2022-08-11 12:59:35 +0100

    qprocessor (not sure if needed)

---
+++ Main.py/qprocessor.py
@@ -0,0 +1,31 @@
+def create_qprocessor(name):
+    """Factory to create a quantum processor for each node in the repeater chain network.
+
+    Has two memory positions and the physical instructions necessary for teleportation.
+
+    Parameters
+    ----------
+    name : str
+        Name of the quantum processor.
+
+    Returns
+    -------
+    :class:`~netsquid.components.qprocessor.QuantumProcessor`
+        A quantum processor to specification.
+
+    """
+    noise_rate = 200
+    gate_duration = 1
+    gate_noise_model = DephaseNoiseModel(noise_rate)
+    mem_noise_model = DepolarNoiseModel(noise_rate)
+    physical_instructions = [
+        PhysicalInstruction(INSTR_X, duration=gate_duration,
+                            quantum_noise_model=gate_noise_model),
+        PhysicalInstruction(INSTR_Z, duration=gate_duration,
+                            quantum_noise_model=gate_noise_model),
+        PhysicalInstruction(INSTR_MEASURE_BELL, duration=gate_duration),
+    ]
+    qproc = QuantumProcessor(name, num_positions=2, fallback_to_nonphysical=False,
+                             mem_noise_models=[mem_noise_model] * 2,
+                             phys_instructions=physical_instructions)
+    return qproc
