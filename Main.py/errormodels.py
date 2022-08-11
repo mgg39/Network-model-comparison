e6c17ea4af81d103f9474fe433b509224de90980
Maria Gragera <m.gragera.garces@gmail.com>
2022-08-11 12:59:06 +0100

    error models (draft later)

---
+++ Main.py/errormodels.py
@@ -0,0 +1,39 @@
+class FibreDepolarizeModel(QuantumErrorModel):
+    """Custom non-physical error model used to show the effectiveness
+    of repeater chains.
+
+    The default values are chosen to make a nice figure,
+    and don't represent any physical system.
+
+    Parameters
+    ----------
+    p_depol_init : float, optional
+        Probability of depolarization on entering a fibre.
+        Must be between 0 and 1. Default 0.009
+    p_depol_length : float, optional
+        Probability of depolarization per km of fibre.
+        Must be between 0 and 1. Default 0.025
+
+    """
+
+    def __init__(self, p_depol_init=0.009, p_depol_length=0.025):
+        super().__init__()
+        self.properties['p_depol_init'] = p_depol_init
+        self.properties['p_depol_length'] = p_depol_length
+        self.required_properties = ['length']
+
+    def error_operation(self, qubits, delta_time=0, **kwargs):
+        """Uses the length property to calculate a depolarization probability,
+        and applies it to the qubits.
+
+        Parameters
+        ----------
+        qubits : tuple of :obj:`~netsquid.qubits.qubit.Qubit`
+            Qubits to apply noise to.
+        delta_time : float, optional
+            Time qubits have spent on a component [ns]. Not used.
+
+        """
+        for qubit in qubits:
+            prob = 1 - (1 - self.properties['p_depol_init']) * np.power(
+                10, - kwargs['length']**2 * self.prope
