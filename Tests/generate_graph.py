2a0d5c337275d83c4dfe5a9393bfd4dee378d881
Maria Gragera <m.gragera.garces@gmail.com>
2022-09-20 11:46:50 +0100

    matplotlib commands

--- Tests/generate_graph.py
+++ Tests/generate_graph.py
@@ -3,12 +3,3 @@ import matplotlib.pyplot as plt
 
 var = pd.read_excel('/home/maria/Network-model-comparison/Tests/Network comparison results.xlsx', skiprows=2)
 print(var)
-var.head()
-
-varNew = var[['Topology','Node number','Quantum Fidelity','Classical Fidelity','Quantum Speed','Classical Speed']]
-varNew.head()
-
-plt.plot(var['column name'])
-var.head()
-
-plt.savefig('test.png')
